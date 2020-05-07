c301b32dc92490e7eafc7f90c18a1c8ab323198d
Nuke <rochadcmarcos@gmail.com>
2020-05-07 11:40:42 -0300

    database change

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_07_131330) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "nome_proprios", force: :cascade do |t|
     t.string "nome"
     t.string "genero"
