83d937b305a6440ed529c117b92d35cd7d3f213c
Nuke <rochadcmarcos@gmail.com>
2020-05-30 09:29:19 -0300

    resolve conflict

--- app/controllers/nome_aleatorio_controller.rb
+++ app/controllers/nome_aleatorio_controller.rb
@@ -7,28 +7,11 @@ class NomeAleatorioController < ApplicationController
         @nome = @nome_proprios.sample
         @sobrenome = @sobrenomes.sample 
 
-        #@nome_completo = "#{@nome.nome} #{@sobrenomes.nome}"
         
-        # Teste
-<<<<<<< HEAD
         nome_c = @nome["nome"]
         nome_c.concat(" ")
         nome_c.concat(@sobrenome["nome"])
         
         render json: { "Nome": nome_c } 
-=======
-        # puts @nome["nome"], @sobrenome["nome"}
-        
-<<<<<<< HEAD
-        render json: { "Nome": @nome + " " + @sobrenome } 
->>>>>>> 7f2fbeda140d6a720a5a37e994fb8c45b186d85e
-=======
-        # Teste
-        nome_c = @nome["nome"]
-        nome_c.concat(" ")
-        nome_c.concat(@sobrenome["nome"])
-        
-        render json: { "Nome": nome_c } 
->>>>>>> 9ff2f1a445af1cb3c605f8cd612a2fa03b2aab47
     end
 end
