19caa8b019550c4292075f45b67dc2ce9bd52f82
Nuke <rochadcmarcos@gmail.com>
2020-05-30 09:48:00 -0300

    routes add

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :nome_aleatorio do 
-    collection do 
-      get 'category'
-    end
-  end
+  resources :nome_aleatorio
+
+  match 'nome_aleatorio/:nacionalidade', to: 'nome_aleatorio#category', via: :get
   
   resources :sobrenomes do 
     collection do 
