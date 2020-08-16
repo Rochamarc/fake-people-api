421942186a36e4384ecae04ede747fd87ee7355f
nuke <rochadcmarcos@gmail.com>
2020-08-15 23:44:36 -0300

    add model ssn, person routes, pry-rails development

---
+++ db/migrate/20200815223038_create_ssns.rb
@@ -0,0 +1,10 @@
+class CreateSsns < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ssns do |t|
+      t.string :number
+      t.references :person, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_165514) do
+ActiveRecord::Schema.define(version: 2020_08_15_223038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,4 +30,21 @@ ActiveRecord::Schema.define(version: 2020_08_13_165514) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "people", force: :cascade do |t|
+    t.string "full_name"
+    t.string "nationality"
+    t.date "birthdate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "ssns", force: :cascade do |t|
+    t.string "number"
+    t.bigint "person_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["person_id"], name: "index_ssns_on_person_id"
+  end
+
+  add_foreign_key "ssns", "people"
 end
