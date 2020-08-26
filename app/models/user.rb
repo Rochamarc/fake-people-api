6484fb90bf5fc31ce174a1c6de8a094f3558425a
nuke <rochadcmarcos@gmail.com>
2020-08-26 18:43:01 -0300

    model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_one :profile
+  has_one :ssn
+  has_one :identity
 end
