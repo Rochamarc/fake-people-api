0e1913efd577b375f623db48d5f234d04708f015
nuke <rochadcmarcos@gmail.com>
2020-09-04 04:22:52 -0300

    Fix v1 controlllers

--- app/controllers/random_names_controller.rb
+++
@@ -1,9 +0,0 @@
-class RandomNamesController < ApplicationController
-  def index
-    @first_name = FirstName.all.sample
-    @last_name = LastName.all.sample 
-
-    render json: { first_name: @first_name, last_name: @last_name }
-
-  end
-end
