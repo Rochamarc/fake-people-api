b8da5c282dcb32ccbf17183c2a67d4614be48458
nuke <rochadcmarcos@gmail.com>
2020-09-04 04:20:46 -0300

    feat(controller): add version 1 to the controller

---
+++ app/controllers/api/v1/random_names_controller.rb
@@ -0,0 +1,13 @@
+module Api 
+  module V1
+    class RandomNamesController < ApplicationController
+      def index
+        @first_name = FirstName.all.sample
+        @last_name = LastName.all.sample 
+      
+        render json: { first_name: @first_name, last_name: @last_name }
+      
+      end
+    end
+  end
+end
