cffd5f4b49de212e2b220b0683dbd743e7043312
nuke <rochadcmarcos@gmail.com>
2020-09-11 17:47:13 -0300

    feat(profile): render ssn and identity on the profile

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,3 +1,8 @@
 class ProfileSerializer < ActiveModel::Serializer
   attributes :id, :name, :nationality, :birthdate
+  belongs_to :user
+
+  def user
+    object.user.as_json(include: [:ssn, :identity], only: [:email, :name])
+  end
 end
