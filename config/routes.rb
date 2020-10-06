92a9d7b6ca3685d6e917aebd6750978a30ae92d1
nuke <rochadcmarcos@gmail.com>
2020-10-06 15:26:29 -0300

    fix: route changes, cors request allowd

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do 
-      resources :identities
-      resources :ssns
- 
+
+    # V1
+    namespace :v1 do
+      resources :identities, except: [:index]
+      resources :ssns, except: [:index]
+
       resources :profiles
       # patch '/profiles', to: "profiles#update", as: 'profile_edit_path' # Esta merda nao esta funcionndo
-  
-      resources :random_names, only: :index # Somente permite um metodo 
+
+      # resources :random_names, only: :index # Somente permite um metodo
       resources :last_names
       resources :first_names
     end
 
+    # V2
     namespace :v2 do
       resources :posts
-      
+
       resources :profiles
     end
   end
@@ -22,33 +25,33 @@ Rails.application.routes.draw do
 
 
   mount_devise_token_auth_for 'User', at: 'auth'
-  
+
   # resources :profiles
   # patch '/profiles', to: "profiles#update", as: 'profile_edit_path' # Esta merda nao esta funcionndo
-  
-  # resources :random_names, only: :index # Somente permite um metodo 
+
+  # resources :random_names, only: :index # Somente permite um metodo
   # resources :last_names
   # resources :first_names
-  
+
   #  resources :nome_aleatorio do
-  #    collection do 
+  #    collection do
   #      match 'category/:nacionalidade', to: 'nome_aleatorio#category', via: :get
   #     end
   #  end
   #
   #  # match 'nome_aleatorio/:nacionalidade', to: 'nome_aleatorio#category', via: :get
-  #  
-  #  resources :sobrenomes do 
-  #    collection do 
+  #
+  #  resources :sobrenomes do
+  #    collection do
   #      match 'category/:nacionalidade', to: 'sobrenomes#category', via: :get
   #    end
   #  end
-  #  
-  #  resources :nome_proprios do 
+  #
+  #  resources :nome_proprios do
   #    collection do
   #      match 'category/:nacionalidade', to: 'nome_proprios#category', via: :get
   #    end
   #  end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
